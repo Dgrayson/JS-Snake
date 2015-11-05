57a072b2c093b035040b716fb9f2e844a0ef2256
Darayavaus Grayson <dgrayson100@gmail.com>
2015-11-05 15:46:11 -0500

    added player class

--- game.js
+++ game.js
@@ -3,10 +3,26 @@ var ctx;
 
 var x = 0; 
 var y = 0; 
+var s; 
+
+var Player = function(){
+	this.playerX = 0; 
+	this.playerY = 0; 
+}
+
+Player.prototype.drawPlayer = function(){
+
+	ctx.beginPath();
+	ctx.rect(this.playerX, this.playerY,10,5); 
+	ctx.closePath(); 
+	ctx.fillStyle = "white"; 
+	ctx.fill(); 
+}
 
 function init(){
 	c = document.getElementById("myCanvas"); 
 	ctx = c.getContext("2d"); 
+	s = new Player(); 
 	return setInterval(draw, 10);
 }
 
@@ -25,13 +41,12 @@ function draw(){
 
 	ctx.fill(); 
 
-	ctx.beginPath();
-	ctx.rect(x,y,10,5); 
-	ctx.closePath(); 
-	ctx.fillStyle = "white"; 
-	ctx.fill(); 
-	console.log("X = " + x); 
-	console.log("Y = " + y); 
+
+	s.drawPlayer(); 
+
+
+	console.log("X = " + s.playerX); 
+	console.log("Y = " + s.playerY); 
 }
 
 function getKeyDown(e){
@@ -39,23 +54,23 @@ function getKeyDown(e){
 	switch(e.keyCode){
 		// up arrow
 		case 38: 
-			if(y > 0)
-				y = y - 5; 
+			if(s.playerY > 0)
+				s.playerY = s.playerY - 5; 
 			break; 
 		// Down arrow
 		case 40:
-			if(y < 145)
-				y = y + 5; 
+			if(s.playerY < 145)
+				s.playerY = s.playerY + 5; 
 			break; 
 		// left arrow
 		case 37: 
-			if(x > 0)
-				x = x - 5; 
+			if(s.playerX > 0)
+				s.playerX = s.playerX - 5; 
 			break; 
 		// right arrow
 		case 39: 
-			if(x < 290)
-				x = x + 5; 
+			if(s.playerX < 290)
+				s.playerX = s.playerX + 5; 
 			break; 
 	}
 }
